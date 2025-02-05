3660f851ac8dcc13d1158bf151ec71ece81aeb86
dorokhinvitaliy <dorokhin2006@bk.ru>
2025-02-05 16:47:45 +0300

    First changes

---
+++ main.py
@@ -0,0 +1,52 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    import cv2
+    import mediapipe as mp
+    import pyautogui
+
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7)
+    mp_draw = mp.solutions.drawing_utils
+
+    cap = cv2.VideoCapture(0)
+    screen_width, screen_height = pyautogui.size()
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        frame = cv2.flip(frame, 1)
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        result = hands.process(rgb_frame)
+        h, w, _ = frame.shape
+
+        if result.multi_hand_landmarks:
+            for hand_landmarks in result.multi_hand_landmarks:
+                x = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x
+                y = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y
+
+                cursor_x = int(x * screen_width)
+                cursor_y = int(y * screen_height)
+                pyautogui.moveTo(cursor_x, cursor_y)
+
+                mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+        cv2.imshow("Hand Tracking", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
